2711d301598d69fadf96f75b7faf48d3a0b4b6c6
hieu0379175265 <72654294+hieu0379175265@users.noreply.github.com>
2022-05-09 20:55:19 +0700

    hmm

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'yêu anh không bae.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/buongduocrui/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
